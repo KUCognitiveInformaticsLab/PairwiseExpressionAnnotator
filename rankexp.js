ec30b386844b96d5100a90db7ea9792bc49dbe83
Dongagent <yds343700931@gmail.com>
2023-02-24 23:53:23 +0900

    ver1.0 is released.

--- rankexp.js
+++ rankexp.js
@@ -1,14 +1,10 @@
-
-// ULID for every subject
-// const ULID = require('ulid');
-// ULID.ulid();
-// console.log(ULID);
-
 let curRating;
 let curID;
 
 curID = 99999;
-const base_url = '/static/experiments/current/nikola_hitl_2nd/stimuli/'
+const base_url = '/static/experiments/current/nikola_hitl_2nd/stimuli/happiness_selected_imgonly100/'
+var experimentCount = 0;
+
 
 // Add your own javacript functions here. 
 function toggle_instruction(page){
@@ -32,7 +28,7 @@ function generateUUID() {
     return uuid;
   }
 
-function get_current_ID() {
+function getCurrentID() {
     const IDTextBox = document.getElementById('uuid');
     if(IDTextBox.value !== null && IDTextBox.value !== '') {
         curID = IDTextBox.value;
@@ -42,10 +38,10 @@ function get_current_ID() {
 
 function showID() {
     const idp = document.getElementById('showID')
-    idp.innerText = get_current_ID()
+    idp.innerText = getCurrentID()
 }
 
-function show_result() {
+function showResult() {
 	get_current_rating();
 
     curResult = document.getElementById("curResult");
@@ -56,30 +52,93 @@ function show_result() {
 //     $("#partcipant_id").val("red"); // テキストボックスに値を設定
 //   });
 
-function clean_radio_btn() {
+let part_consent = false;
+
+function submitForm() {
+    // Get the form data
+    const form = document.getElementById("my-form");
+
+    // Check if the checkbox is checked
+    const checkbox = form.querySelector('input[name="consent"]');
+    if (!checkbox.checked) {
+      document.getElementById("error-message").textContent = "You must check the box to continue.";
+      return;
+    }
+    part_consent = checkbox.checked;
+    console.log(part_consent);
+    window.toggle_instruction('id_page'); 
+    generateUUID();
+}
+
+function checkInfo() {
+    participant_id = document.getElementById("participant_id").value;
+    if (!participant_id) {  // if participant_id is empty
+        document.getElementById("error-message-info").textContent = "You must enter your participant ID to continue.";
+        return;
+    }
+
+    age_num = document.getElementById("age").value;
+    if (!age_num) {  // if age is empty
+        document.getElementById("error-message-info").textContent = "You must enter your age to continue.";
+        return;
+    }
+
+    // finish checking, go to practice page
+    window.toggle_instruction('practice_page'); 
+    practiceTest();
+}
+
+
+function cleanRadioBtn() {
 	$(`input[name="rating"]`).prop("checked", false);
     document.getElementById("curResult").innerText = ""
 };
-window.clean_radio_btn = clean_radio_btn;
+window.cleanRadioBtn = cleanRadioBtn;
 
 // -----------------
 // Experiment
 // Define a function to compare two images and ask the user to choose
-// The compareImages function returns a promise that resolves to either -1 
-// if the user thinks the right image is greater, 0 if the user thinks both images are equal, 
-// or 1 if the user thinks the left image is greater.
-async function compareImages(img1, img2, pairs) {
+// The compareImages function returns a promise that resolves to 
+// either -1 if the user thinks the left image is greater
+// or 1 if the user thinks the right image is greater.
+async function compareImages(img1, img2, pairs, containerName) {
     // Hide all other images while the comparison is being made
-    const images = Array.from(document.querySelectorAll('#experiment-image-container img'));
+    var images = document.querySelectorAll(`#${containerName} img`);
+    let numberElement;
+    if (document.getElementById('experiment_page').classList.contains('hidden')) {
+        numberElement = document.getElementById('experimentCounter_practice');
+    } else {
+        numberElement = document.getElementById('experimentCounter_rating');
+    }
+
     images.forEach(img => {
         if (img !== img1 && img !== img2) {
-        img.classList.add('hidden');
+            img.classList.add('hidden');
         }
     });
 
     return new Promise(resolve => {
         // Create a new div to show the two images
         const comparisonDiv = document.createElement('div');
+        comparisonDiv.style.display = 'flex'; // add this line to make the images display side-by-side
+        comparisonDiv.style.justifyContent = 'center'; // add this line to center the images
+        comparisonDiv.style.alignItems = 'center'; // add this line to center the images
+        comparisonDiv.style.marginBottom = '20px'; // add some margin to the bottom of the div
+        img1.style.marginLeft = '20px'; // add some margin to the left of img1
+        img1.style.marginRight = '20px'; // add some margin to the right of img1
+        img2.style.marginLeft = '20px'; // add some margin to the left of img2
+        img2.style.marginRight = '20px'; // add some margin to the right of img2
+        
+        
+        if (document.getElementById('experiment_page').classList.contains('hidden')) {
+            img1.classList.add('practice-image');
+            img2.classList.add('practice-image');
+        } else {
+            img1.classList.add('image');
+            img2.classList.add('image');
+        }
+
+
         comparisonDiv.appendChild(img1);
         comparisonDiv.appendChild(img2);
         document.body.appendChild(comparisonDiv);
@@ -87,34 +146,73 @@ async function compareImages(img1, img2, pairs) {
         // Attach click handlers to the images to get user input
         
         img1.onclick = () => {
-        document.body.removeChild(comparisonDiv);
+            document.body.removeChild(comparisonDiv);
 
-        // Restore all hidden images
-        images.forEach(img => img.classList.remove('hidden'));
+            // Restore all hidden images
+            images.forEach(img => img.classList.remove('hidden'));
 
-        pairs.push([img1.id, img2.id, -1]);
-        resolve(-1);
+            pairs.push([img1.id, img2.id, -1]);
+            // add 1 to counter
+            experimentCount++;
+            numberElement.textContent = experimentCount;
+            resolve(-1);
         };
         img2.onclick = () => {
-        document.body.removeChild(comparisonDiv);
+            document.body.removeChild(comparisonDiv);
 
-        // Restore all hidden images
-        images.forEach(img => img.classList.remove('hidden'));
+            // Restore all hidden images
+            images.forEach(img => img.classList.remove('hidden'));
 
-        pairs.push([img1.id, img2.id, 1]);
-        resolve(1);
+            pairs.push([img1.id, img2.id, 1]);
+            // add 1 to counter
+            experimentCount++;
+            numberElement.textContent = experimentCount;
+            resolve(1);
         };
+
+        // Attach keydown handler to document to get user input
+        function selectImage(selectedImg, otherImg, result) {
+            // Remove the comparison div
+            document.body.removeChild(comparisonDiv);
+  
+            // Restore all hidden images
+            images.forEach(img => img.classList.remove('hidden'));
+  
+            pairs.push([selectedImg.id, otherImg.id, result]);
+            
+            // add 1 to counter
+            experimentCount++;
+            numberElement.textContent = experimentCount;
+
+            // destroy event listener once we have a response
+            document.removeEventListener("keydown", handleKeydown)
+          }
+  
+        function handleKeydown(event) {
+        if (event.key === "q") { // Q for selecting img1
+            console.log(event.key);
+            selectImage(img1, img2, -1);
+            resolve(-1);
+        } else if (event.key === "p") { // P for selecting img2
+            console.log(event.key);
+            selectImage(img1, img2, 1);
+            resolve(1);
+        }
+        }
+  
+        // Attach keydown handler to document to get user input
+        document.addEventListener("keydown", handleKeydown);
     });
 }
-  
+
 // Define a function to sort an array of images
-async function mergeSort(images) {
+async function mergeSort(images, containerName) {
     const pairs = [];
-    var sortedImages = await mergeSortRecursive(images, pairs);
+    var sortedImages = await mergeSortRecursive(images, pairs, containerName);
     return { images, pairs, sortedImages};
 }
   
-async function mergeSortRecursive(images, pairs) {
+async function mergeSortRecursive(images, pairs, containerName) {
     // Base case: if there is only one image, return it
     if (images.length <= 1) {
         return images;
@@ -126,14 +224,14 @@ async function mergeSortRecursive(images, pairs) {
     const right = images.slice(mid);
 
     // Recursively sort the left and right sub-arrays
-    const sortedLeft = await mergeSortRecursive(left, pairs);
-    const sortedRight = await mergeSortRecursive(right, pairs);
+    const sortedLeft = await mergeSortRecursive(left, pairs, containerName);
+    const sortedRight = await mergeSortRecursive(right, pairs, containerName);
 
     // Merge the sorted sub-arrays together
     const merged = [];
     let i = 0, j = 0;
     while (i < sortedLeft.length && j < sortedRight.length) {
-        const cmp = await compareImages(sortedLeft[i], sortedRight[j], pairs);
+        const cmp = await compareImages(sortedLeft[i], sortedRight[j], pairs, containerName);
         if (cmp === 1) {
         merged.push(sortedRight[j++]);
         } else {
@@ -150,15 +248,42 @@ async function mergeSortRecursive(images, pairs) {
 }
 
 function practiceTest() {
-mergeSort(Array.from(document.querySelectorAll('#practice-image-container img')))
-    .then(result => {
-        console.log('Pairwise comparisons:');
-        result.pairs.forEach(pair => {
-        console.log(`${pair[0]} vs. ${pair[1]}: ${pair[2]}`);
-        });
-        // show the button
-        document.getElementById("practice_success").style.display = "block";
+    // initialize the counter
+    experimentCount = 0;
+
+    mergeSort(Array.from(document.querySelectorAll('#practice-image-container img')), 'practice-image-container')
+        .then(result => {
+            console.log('Pairwise comparisons:');
+            result.pairs.forEach(pair => {
+            console.log(`${pair[0]} vs. ${pair[1]}: ${pair[2]}`);
+            });
+            // show the button
+            document.getElementById("practice_success").style.display = "block";
+
+            // Show the sorted images
+            const sortedContainer = document.createElement('div');
+            sortedContainer.id = 'practice-sorted-container';
+            sortedContainer.classList.add('practice-sorted-container');
+            sortedImgList = result.sortedImages;
+
+            // Show the accending order of the images
+            result.sortedImages.reverse().forEach(img => {
+                img.onclick = null;
+                img.cursor = 'none';
+                img.classList.add('practice-sorted-container');
+                img.classList.add('image');
+                
+                sortedContainer.appendChild(img);
+            });
+            document.body.appendChild(sortedContainer);
     });
+}
+
+function destroyPractice() {
+    // destroy the practice
+    document.getElementById("practice-image-container").innerHTML = '';
+    document.getElementById("practice-sorted-container").innerHTML = '';
+    document.getElementById("practice_success").style.display = "none";
 }
 
 function shuffleArray(array) {
@@ -175,6 +300,10 @@ var finalResult = [];
 var sortedImgList = [];
 
 function startExperiment() {
+    // initialize the counter
+    finalResult = [];
+    sortedImgList = [];
+    experimentCount = 0;
     // load the images
     var imageContainer = document.getElementById("experiment-image-container");
     // clear the container
@@ -182,14 +311,16 @@ function startExperiment() {
 
     var images = [];
 
-    // generate image names dynamically
-    for (var i = 1; i <= 100; i++) {
-        images.push(base_url + "image_" + i + ".png"); // TODO change the image name
-    }
+
+    // // generate image names dynamically
+    // for (var i = 1; i <= 100; i++) {
+    //     images.push(base_url + "image_" + i + ".png"); // TODO change the image name
+    // }
 
     // current image index
     images = ['ha_367.png', 'ha_84.png', 'ha_207.png', 'ha_2.png', 'ha_212.png', 'ha_399.png', 'ha_428.png', 'ha_172.png', 'ha_50.png', 'ha_1.png', 'ha_407.png', 'ha_201.png', 'ha_215.png', 'ha_5.png', 'ha_348.png', 'ha_54.png', 'ha_412.png', 'ha_174.png', 'ha_404.png', 'ha_202.png', 'ha_43.png', 'ha_377.png', 'ha_149.png', 'ha_106.png', 'ha_24.png', 'ha_476.png', 'ha_310.png', 'ha_489.png', 'ha_477.png', 'ha_311.png', 'ha_19.png', 'ha_107.png', 'ha_139.png', 'ha_111.png', 'ha_33.png', 'ha_27.png', 'ha_475.png', 'ha_32.png', 'ha_110.png', 'ha_128.png', 'ha_262.png', 'ha_288.png', 'ha_303.png', 'ha_301.png', 'ha_315.png', 'ha_329.png', 'ha_249.png', 'ha_314.png', 'ha_466.png', 'ha_116.png', 'ha_11.png', 'ha_443.png', 'ha_292.png', 'ha_244.png', 'ha_442.png', 'ha_132.png', 'ha_130.png', 'ha_124.png', 'ha_454.png', 'ha_497.png', 'ha_252.png', 'ha_284.png', 'ha_482.png', 'ha_131.png', 'ha_119.png', 'ha_451.png', 'ha_243.png', 'ha_257.png', 'ha_256.png', 'ha_16.png', 'ha_450.png', 'ha_122.png', 'ha_320.png', 'ha_334.png', 'ha_491.png', 'ha_268.png', 'ha_296.png', 'ha_241.png', 'ha_233.png', 'ha_384.png', 'ha_186.png', 'ha_151.png', 'ha_145.png', 'ha_184.png', 'ha_59.png', 'ha_9.png', 'ha_393.png', 'ha_344.png', 'ha_422.png', 'ha_185.png', 'ha_152.png', 'ha_368.png', 'ha_235.png', 'ha_221.png', 'ha_382.png', 'ha_396.png', 'ha_341.png', 'ha_380.png', 'ha_381.png', 'ha_342.png']
-    
+    // DEBUG
+    // images = images.slice(0, 5);
 
     var shuffledImages = shuffleArray(images);
 
@@ -201,27 +332,20 @@ function startExperiment() {
         imageContainer.appendChild(img);
     }
 
-
     // Sort the images and display the results
-    mergeSort(Array.from(document.querySelectorAll('#experiment-image-container img')))
+    mergeSort(Array.from(document.querySelectorAll('#experiment-image-container img')), 'experiment-image-container')
         .then(result => {
-        // Show the sorted images
-        const sortedContainer = document.createElement('div');
-        sortedImgList = result.sortedImages;
 
-        // Show the accending order of the images
-        result.sortedImages.reverse().forEach(img => {
-            img.onclick = null;
-            img.cursor = 'default';
-            
-            sortedContainer.appendChild(img);
-        });
-        document.body.appendChild(sortedContainer);
+        // get sorted img list
+        sortedImgList = result.sortedImages;
 
         // Show the "experiment finished" message
         const finishMessage = document.createElement('h2');
         finishMessage.textContent = 'Experiment finished. Thank you!';
         document.body.appendChild(finishMessage);
+
+        // show the button
+        document.getElementById("experiment_success").style.display = "block";
 
         // Display the pairwise comparisons made by the user
         console.log('Pairwise comparisons:');
@@ -229,14 +353,33 @@ function startExperiment() {
             console.log(`${pair[0]} vs. ${pair[1]}: ${pair[2]}`);
             finalResult.push(pair);
         });
-
-
     });
 }
 
+function showExpResult() {
+    // Show the sorted images
+    const sortedContainer = document.createElement('div');
+    sortedContainer.classList.add('practice-sorted-container');
+
+    // Show the decending order of the images
+    sortedImgList.forEach(img => {
+        img.onclick = null;
+        img.cursor = 'default';
+        
+        sortedContainer.appendChild(img);
+    });
+    document.body.appendChild(sortedContainer);
+}
+
 function get_my_result() {
+    var age = document.getElementById("age").value;
+    var area = document.getElementById("area").value;
+    var participant_id = document.getElementById("participant_id").value;
     result = {
+        'participant_id' : participant_id,
         'ID' : curID,
+        'age' : age,
+        'area' : area,
         'pairwise_rating' : finalResult,
         'sortedlist' : sortedImgList,
     }
@@ -245,7 +388,7 @@ function get_my_result() {
 }
 
 function my_submit() {
-    get_current_ID();
+    getCurrentID();
     var result = get_my_result()
 	submit_data(result)
 };
